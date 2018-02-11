e68f0e4c88ba0e59ac39b5b3b7f64315556060a5
Jorge Avila <avilajorge314@gmail.com>
2018-02-10 17:21:11 -0800

    Initial commit for SongTest class

---
+++ app/src/androidTest/java/com/example/maxvoskr/musicplayer/SongTest.java
@@ -0,0 +1,31 @@
+package com.example.maxvoskr.musicplayer;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by avila on 2/10/2018.
+ */
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class SongTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.maxvoskr.musicplayer", appContext.getPackageName());
+    }
+
+}
