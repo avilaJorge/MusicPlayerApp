a9b46cd891acc3cbfd0f48f5c830b55aec1202c7
Timothy <timothy.l.walker1@gmail.com>
2018-02-18 01:06:33 -0800

    Added tests for user story scenarios

--- app/src/androidTest/java/com/example/maxvoskr/musicplayer/UserStoryOneTest.java
+++ app/src/androidTest/java/com/example/maxvoskr/musicplayer/UserStoryOneTest.java
@@ -1,7 +1,14 @@
 package com.example.maxvoskr.musicplayer;
 
 
+
+
+
+
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.rule.GrantPermissionRule;
@@ -15,6 +22,7 @@ import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 import org.hamcrest.core.IsInstanceOf;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,29 +31,47 @@ import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withResourceName;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.example.maxvoskr.musicplayer.MusicArrayList.musicList;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class UserStoryOneTest {
 
+    private static Song song;
+
+
     @Rule
     public ActivityTestRule<LoadingActivity> mActivityTestRule = new ActivityTestRule<>(LoadingActivity.class);
+
 
 
     @Rule
     public GrantPermissionRule permissionRule = GrantPermissionRule.grant(Manifest.permission.ACCESS_FINE_LOCATION);
 
+
+    @Before
+    public void before(){
+
+        song = musicList.get(0);
+
+    }
+
+
     @Test
-    public void UserSelectsATrackToPlay() {
+    public void userSelectsATrackToPlay() {
+
 
         /*
             Given: app is open
@@ -63,19 +89,9 @@ public class UserStoryOneTest {
                 .atPosition(0)
                 .perform(click());
 
-        // check that we have switched
-        onData(allOf(is(instanceOf(String.class)), is("Last Played:")));
+        onView(withText(song.getName())).check(matches(isDisplayed()));
 
-        //
-
-
-
-        // Perform switch to flashBack mode
-        //onView(withId(R.id.flashbackMode)).perform(click());
-
-        // Check for Last played object
-        //onData(allOf(is(instanceOf(String.class)), is("Last Played:")));
-
+        //onData(allOf(is(instanceOf(String.class)), is(song.getName()))).check(matches(isDisplayed()))    ;
     }
 
 
--- app/src/androidTest/java/com/example/maxvoskr/musicplayer/UserStoryThreeTest.java
+++ app/src/androidTest/java/com/example/maxvoskr/musicplayer/UserStoryThreeTest.java
@@ -10,24 +10,35 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
+
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.example.maxvoskr.musicplayer.MusicArrayList.albumList;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 
+
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class UserStoryThreeTest {
+
+    private static Song song;
+    private static Album album;
 
     @Rule
     public ActivityTestRule<LoadingActivity> mActivityTestRule = new ActivityTestRule<>(LoadingActivity.class);
@@ -35,6 +46,16 @@ public class UserStoryThreeTest {
 
     @Rule
     public GrantPermissionRule permissionRule = GrantPermissionRule.grant(Manifest.permission.ACCESS_FINE_LOCATION);
+
+    @Before
+    public void before(){
+
+        album = albumList.get(0);
+        song = album.getMusicList().get(0);
+
+    }
+
+
 
     @Test
     public void UserNavigatesfromPlaybackToFlashBack() {
@@ -47,31 +68,26 @@ public class UserStoryThreeTest {
 
         */
 
-        // App should be open, check for song list mode and click first song
-        onData(anything()).inAdapterView(withId(R.id.trackList))
-                .atPosition(0)
-                .perform(click());
+        onView(withId(R.id.flashbackMode)).perform(click());
 
-        // check that we have switched
-        onData(allOf(is(instanceOf(String.class)), is("Last Played:")));
-
-        //
-
-
-
-        // Perform switch to flashBack mode
-        //onView(withId(R.id.flashbackMode)).perform(click());
-
-        // Check for Last played object
-        //onData(allOf(is(instanceOf(String.class)), is("Last Played:")));
+        onView(withText("Last Played:")).check(matches(isDisplayed()));
 
     }
 
     @Test
     public void UserNavigatesFromFlashbackToPlayback() {
 
+        onView(withId(R.id.flashbackMode)).perform(click());
 
+        onView(withText("Last Played:")).check(matches(isDisplayed()));
 
+        onView(withId(R.id.songsMode)).perform(click());
+
+        onData(anything()).inAdapterView(withId(R.id.trackList))
+                .atPosition(0)
+                .perform(click());
+
+        onView(withText(song.getName())).check(matches(isDisplayed()));
     }
 
 
