765d06e7d5613a442ace2b05e322416e139a7d90
Jorge Avila <avilajorge314@gmail.com>
2018-02-17 22:20:43 -0800

    Added all other song files, changed - to _ in file names, and added a single import to AlbumAdapter to get code to compile.  Encountered a null object reference error in AlbumAdapter.java:74

--- app/src/main/java/com/example/maxvoskr/musicplayer/AlbumListActivity.java
+++ app/src/main/java/com/example/maxvoskr/musicplayer/AlbumListActivity.java
@@ -14,6 +14,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
