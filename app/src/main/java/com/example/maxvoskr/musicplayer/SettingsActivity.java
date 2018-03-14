45d45ef19ca0c900c12edde11637b5b1c7c99ff8
tem002 <tem002@ucsd.edu>
2018-03-13 20:30:48 -0700

    Created sorting in song mode

--- app/src/main/java/com/example/maxvoskr/musicplayer/SettingsActivity.java
+++ app/src/main/java/com/example/maxvoskr/musicplayer/SettingsActivity.java
@@ -82,8 +82,6 @@ public class SettingsActivity extends AppCompatActivity implements
 
         ArrayList<Song> downloadableSongs = MusicArrayList.allMusicList;
         for(Song downloaded : MusicArrayList.musicList) {
-            Toast.makeText(getApplicationContext(), downloaded.getName() + " was removed",
-                    Toast.LENGTH_SHORT).show();
             downloadableSongs.remove(downloaded);
         }
 
